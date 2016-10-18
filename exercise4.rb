58ac1aa0c2aff413a1613289e658e132b2fb04ac
David Wood <davidnmwood@gmail.com>
2016-10-18 15:58:08 -0500

    control structures exercises

---
+++ exercise4.rb
@@ -0,0 +1,71 @@
+# if 2 > 1
+#   puts "I get printed!"
+# end
+
+# if 0 > 1
+#   puts "I will not get printed!"
+# end
+# #
+# number = 3
+#
+# if number > 0
+#   puts "#{number} is positive" #this line of code will be executed :)
+# else
+#   puts "#{number} is negative"
+# end
+#
+
+# x = 3
+# y = 4
+#
+# # if x > y
+# #   puts "x is greater than y!"
+# # elseif x < y
+# #   puts "x is less than y!" #this line will be executed :)
+# # else
+# #   puts "x equals y!"
+# # end
+#
+# #
+# # if x != 10
+# #   puts "I get printed!"
+# # end
+#
+# unless x == 10
+#   puts "I get printed!"
+# end
+#
+# #
+# counter = 1
+# while counter < 4
+#   puts "counter currently at #{counter}"
+# end
+#
+# while true
+#   puts "I'm an infinite loop!" #weeeeeeeeeeee!
+# end
+#
+# counter = 3
+# until counter == 0
+#   puts "counter currently at #{counter}"
+#   count -= 1
+# end
+#
+# until false
+#   puts "I'm an infinite loop!"
+# end
+# #
+# # 7.times do
+# #   puts "Chunky bacon!"
+# # end
+# # 3.times { puts "Chunky bacon!" }
+# # #
+# # one_to_five = (1..5)
+# # one_to_five.each do |num|
+# #   puts num
+# # end
+#
+# one_to_five = (1..5)
+# one_to_five.each do |num|
+#   puts (num**2).to_s
+# end
