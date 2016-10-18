cf3fe20aa3e3c83d6df84764e9f03aee7f679d38
David Wood <davidnmwood@gmail.com>
2016-10-18 13:29:37 -0500

    hello world

---
+++ fundamentals.rb
@@ -0,0 +1,28 @@
+# if statements
+
+# if some condition is true
+# => run this code here!
+# otherwise (optional
+# => let's do this other thing
+#
+# count = 9
+#
+# puts "not equal to 10!" unless count == 10
+#
+
+# score = 90
+#
+# while score < 100
+#   puts "Current score: #{score}"
+#   # score = score + 1
+#   score += 1
+# end
+
+#
+# 10.times do  |number|
+#   puts number
+# end
+
+(10..19).each do  |cats|
+  puts "There are #{cats} cats"
+end
