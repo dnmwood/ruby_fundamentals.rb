680cdc38e69c50cf0fc0bcbb4fd447151e67bf5b
David Wood <davidnmwood@gmail.com>
2016-10-18 15:14:23 -0500

    part three calculating age

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,14 @@
-puts "what is your name, buddy?"
-#prompt
+puts "what is your name, buddy?" #prompt
 name = gets.chomp
 
-puts "Hi #{name}!"
+puts "Hi #{name}!" #response
+
+puts "How old are you?" #2nd prompt
+
+age = gets.chomp
+new_age = age.to_i
+year = (2016 - new_age)
+#turning "new_age" into integer
+#calculating current year - new_age
+
+puts "Wow, you were born in #{year}, you're so old, buddy!" #2nd response
