b6522a9dacf3f8b92f44c9770128d94b2836d422
David Wood <davidnmwood@gmail.com>
2016-10-18 14:46:07 -0500

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,26 @@
+puts "#{55 * 1.15}" #$55 + tip
+
+
+
+
+
+tip = 55 * 1.17
+puts tip
+
+total = 64.35 #$55 + tip
+
+total.to_s #converting 64.35 to string
+
+puts total #shows total
+
+
+
+
+puts "The number #{45628*7839} is quite big!"
+#interpolation!
+
+
+
+
+(10 < 20 && 30 < 20) || !(10 == 11)
+#true: 10 does not NOT equal 11
