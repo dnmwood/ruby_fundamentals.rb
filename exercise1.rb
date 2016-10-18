a62f474f909f02aa21dd84f095051348317f3f09
David Wood <davidnmwood@gmail.com>
2016-10-18 13:45:45 -0500

    Escape Sequences

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,8 @@
 # puts "Betty's pie shop"
 
-puts "Ada Lovelace lived for #{1852 - 1815} years."
+# puts "Ada Lovelace lived for #{1852 - 1815} years."
+# puts "Hello\t\world"
+#
+# # puts "Hello\b\b\b\bGoodbye World"
+# puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
